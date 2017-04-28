aafa19a2b0ea72288085ceeaaf0cd5487b6c2422
yanniks <yannik@yanniks.de>
2017-04-28 15:11:14 +0200

    Add Date extension

---
+++ Extensions/Date.swift
@@ -0,0 +1,24 @@
+//
+//  Date.swift
+//  DHBW Stuttgart
+//
+//  Created by Yannik Ehlert on 28.04.17.
+//  Copyright © 2017 Yannik Ehlert. All rights reserved.
+//
+
+import Foundation
+
+public extension Date {
+    func timeIsAfter(date: Date) -> Bool {
+        var dateComponentCompare1 = Calendar.current.dateComponents([.hour, .minute, .weekday], from: Date())
+        var dateComponentCompare2 = Calendar.current.dateComponents([.hour, .minute, .second], from: Date())
+        let dateComponent1 = Calendar.current.dateComponents([.hour, .minute, .second], from: self)
+        let dateComponent2 = Calendar.current.dateComponents([.hour, .minute, .second], from: date)
+        dateComponentCompare1.hour = dateComponent1.hour
+        dateComponentCompare2.hour = dateComponent2.hour
+        dateComponentCompare1.minute = dateComponent1.minute
+        dateComponentCompare2.minute = dateComponent2.minute
+        
+        return Calendar.current.date(from: dateComponentCompare1)!.timeIntervalSince1970.isLess(than: Calendar.current.date(from: dateComponentCompare2)!.timeIntervalSince1970)
+    }
+}
